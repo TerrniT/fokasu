73a2e991d4bafeebb958c2b56cae2ba4f8f305d6
TerrniT <terrnit.megaman@gmail.com>
2023-02-22 22:53:22 +0500

    feat(init): init project

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,17 @@
+#![cfg_attr(
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
+)]
+
+// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+#[tauri::command]
+fn greet(name: &str) -> String {
+    format!("Hello, my nigga {}! ", name)
+}
+
+fn main() {
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![greet])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import { invoke } from "@tauri-apps/api/tauri";
+
+function App() {
+  const [greetMsg, setGreetMsg] = useState("");
+  const [name, setName] = useState("");
+
+  async function greet() {
+    setGreetMsg(await invoke("greet", { name }));
+  }
+
+  return (
+    <div className="" onClick={greet}>Hello</div>
+  );
+}
+
+export default App;
